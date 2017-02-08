1b2766a810bfe4a5a34df3306246ef5fefd035bd
Jason Li <jasonli2310@gmail.com>
2017-02-08 14:12:34 -0600

    all done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -42,4 +42,31 @@ class PhotosController < ApplicationController
     redirect_to("http://localhost:3000/photos")
 
   end
+
+  def edit_form
+
+    @photo_id = params["id"].to_i
+    @currentphoto = Photo.find(@photo_id)
+    @currentsource = @currentphoto["source"]
+    @currentcaption = @currentphoto["caption"]
+
+
+    render("photos/edit_form.html.erb")
+  end
+
+  def update_row
+    @photo_id = params["id"].to_i
+    @newcaption = params["the_caption"]
+    @newsource = params["the_source"]
+
+    i = Photo.find(@photo_id)
+    i.caption = @newcaption
+    i.source = @newsource
+    i.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get("/photos/:id",       { :controller => "photos", :action => "show" })
   get("/create_photo",     { :controller => "photos", :action => "create_row" })
   get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
+
 
 
 
