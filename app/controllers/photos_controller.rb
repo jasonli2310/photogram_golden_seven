eb361bc3f9aa9a52cd6dd811a2aef37f13e6e23a
Jason Li <jasonli2310@gmail.com>
2017-02-08 13:53:40 -0600

    delete and create complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,5 +16,30 @@ class PhotosController < ApplicationController
     render("photos/show.html.erb")
   end
 
+  def new_form
+    render("photos/new_form.html.erb")
+  end
 
+  def create_row
+
+    @newcaption = params["the_caption"]
+    @newsource = params["the_source"]
+
+    a = Photo.new
+    a.source = @newsource
+    a.caption = @newcaption
+    a.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    @photo_id = params["id"].to_i
+
+    link = Photo.find(@photo_id)
+    link.delete
+
+    redirect_to("http://localhost:3000/photos")
+
+  end
 end
