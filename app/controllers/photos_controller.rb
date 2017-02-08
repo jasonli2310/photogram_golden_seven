7e2a69421a93b3cbd8a9992e140c32bc92c5ca4d
Jason Li <jasonli2310@gmail.com>
2017-02-08 13:21:55 -0600

    done with part 1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,5 +5,16 @@ class PhotosController < ApplicationController
     render("photos/index.html.erb")
   end
 
+  def show
+    @photo_id = params["id"].to_i
+
+    @photolink = Photo.find(@photo_id)
+
+    @display = @photolink["source"]
+    @caption = @photolink["caption"]
+
+    render("photos/show.html.erb")
+  end
+
 
 end
